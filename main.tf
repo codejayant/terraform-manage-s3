22e2a6f64d29a269c85a0ff3b6c3189e89108211
jayant sarkar <codejayant@gmail.com>
2020-07-26 20:20:23 -0700

    add S3 bucket lifecycle policies

--- main.tf
+++ main.tf
@@ -12,6 +12,25 @@ resource "aws_s3_bucket" "my_bucket" {
     enabled = true
   }
 
+  lifecycle_rule {
+    prefix = "files/"
+    enabled = true
+
+    noncurrent_version_transition {
+      days = 30
+      storage_class = "STANDARD_IA"
+    }
+
+    noncurrent_version_transition {
+      days = 60
+      storage_class = "GLACIER"
+    }
+
+    noncurrent_version_expiration {
+      days = 90
+    }
+  }
+
   tags = {
     Type="LOG"
     Tier="STANDARD"
